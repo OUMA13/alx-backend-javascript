a1f9e42f257b44be5048e8a7c7e152ab3588f077
OUMA13 <omaimaelkhanchoufu9@gmail.com>
2024-06-26 23:22:33 +0000

    0x01-ES6_promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,3 @@
-// 0-promise.js
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    const condition = true; // You can change this to false to test the rejection
-    if (condition) {
-      resolve('success');
-    } else {
-      reject(new Error('error'));
-    }
-  });
+  return new Promise(() => {});
 }
-
